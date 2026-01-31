cbe114273bb7edb338384324abe61e95d5d4036f
Arsen-Aghajanyan <aghajanyanars234@gmail.com>
2026-01-31 15:58:16 +0400

    solution

--- countDownTimer.py
+++ countDownTimer.py
@@ -1,20 +1,27 @@
 import time
 
-user_input = input("Insert time to count down (h:m:s): ")
-
-# Split input
-h, m, s = user_input.split(":")
-
 # Convert to seconds
-total_seconds = int(h) * 3600 + int(m) * 60 + int(s)
+def countdown(hours, minutes, seconds):
+    total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds)
 
-while total_seconds >= 0:
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
+    while total_seconds >= 0:
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        seconds = total_seconds % 60
 
-    print(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
-    time.sleep(1)
-    total_seconds -= 1
+        print(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+        time.sleep(1)
+        total_seconds -= 1
+    print("Time's up!")
 
-print("Time's up!")
+def main():
+    user_input = input("Insert time to count down (h:m:s): ")
+
+    try:
+        hours, minutes, seconds = user_input.split(":")
+        countdown(int(hours), int(minutes), int(seconds))
+    except ValueError:
+        print("Invalid input!(h:m:s)")
+
+if __name__ == "__main__":
+    main()
